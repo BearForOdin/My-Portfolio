62db36edb99508edd8253cdaedee386f992984c6
BearForOdin <selasidzeamesi82@gmail.com>
2025-07-23 20:11:07 -0700

    blog-link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href=""
+                href="https://medium.com/@selasidzeamesi82/"
                 target="_blank"
                 rel="noreferrer"
               >
